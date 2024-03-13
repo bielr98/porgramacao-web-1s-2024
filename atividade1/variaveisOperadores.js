72e37cfe04737277ef1bb3f3b2ba271b0c33d6ed
Gabriel Ribeiro Noronha <gabrielrib98@gmail.com>
2024-03-13 10:06:35 -0300

    Atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,15 @@
+const pi = 3.14;
+const a = 3;
+const b = 5;
+
+let c = a + b;
+console.log('O resultado da soma entre ' + a + ' e ' + b + ' é  = ' + c);
+
+c = a - b ;
+console.log('o resultado da subtraçao entre' ${a} e ${b} é ${c}');
+
+c = a * b ;
+console.log('o resultado da Multiplicacao entre S{a} e S{b} é ${c}');
+
+c = a / b ;
+console.log('o resultado da divisao entre S{a} e S{b} é ${c}');
