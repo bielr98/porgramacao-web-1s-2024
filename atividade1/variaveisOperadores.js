922b6a05af9e577ca23e22764285bb675fa8e499
Gabriel Ribeiro Noronha <gabrielrib98@gmail.com>
2024-03-13 10:11:26 -0300

    Ajuste

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -6,10 +6,10 @@ let c = a + b;
 console.log('O resultado da soma entre ' + a + ' e ' + b + ' é  = ' + c);
 
 c = a - b ;
-console.log('o resultado da subtraçao entre' ${a} e ${b} é ${c}');
+console.log(`o resultado da subtraçao entre ${a} e ${b} é ${c}`);
 
 c = a * b ;
-console.log('o resultado da Multiplicacao entre S{a} e S{b} é ${c}');
+console.log(`o resultado da Multiplicacao entre ${a} e ${b} é ${c}`);
 
 c = a / b ;
-console.log('o resultado da divisao entre S{a} e S{b} é ${c}');
+console.log(`o resultado da divisao entre ${a} e ${b} é ${c}`);
