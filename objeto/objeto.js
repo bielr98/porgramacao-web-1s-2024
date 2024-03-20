7af254778b157447e4480d8ff7f48134f2f6526f
Gabriel Ribeiro Noronha <gabrielrib98@gmail.com>
2024-03-20 10:54:23 -0300

    Atividade2 e obj

---
+++ objeto/objeto.js
@@ -0,0 +1,31 @@
+let calculadora = {
+    numeroA: 0,
+    numeroB: 0,
+    somar: function(){
+       return this.numeroA + this.numeroB;
+    }
+};
+
+calculadora.numeroA = 2;
+calculadora.numeroB = 3;
+
+console.log(calculadora.numeroA + calculadora.numeroB);
+console.log(calculadora.somar());
+
+const pessoa = {
+    nome: "Gabriel Ribeiro",
+    cpf: "123456788",
+    telefone: "6199999"
+}
+console.log(pessoa)
+
+for (const key in pessoa) {
+    // console.log(key)
+    console.log(key + ': ' + pessoa[key] )
+}
+
+let arr = [0, 1, 23, 24, 25, 100, 200];
+for (const key in arr) {
+    console.log(key + ': ' + arr[key] )
+}
+
